4c132d695fd0f21c83447f160a8321f7c98d4dd7
sean <770899447@qq.com>
2025-03-20 07:18:29 +0800

    /

--- src/Components/Tabbar/index.tsx
+++ src/Components/Tabbar/index.tsx
@@ -1,24 +1,21 @@
-import { FC, useEffect, useState } from "react";
-import Taro from "@tarojs/taro";
-import classNames from "classnames";
-import { useLoginStore } from "@/store/loginStore";
-import { Image, View } from "@tarojs/components";
-import { useTabbarStore } from "@/store/tabbarStore";
-import styles from "./index.module.scss";
+import { Image, View } from '@tarojs/components';
+import Taro from '@tarojs/taro';
+import classNames from 'classnames';
+import { FC, useEffect, useState } from 'react';
+import { useLoginStore } from '@/store/loginStore';
+import { useTabbarStore } from '@/store/tabbarStore';
+import styles from './index.module.scss';
 
 const LoginButton: FC<{ className?: string }> = ({ className }) => {
   const login = () => {
     Taro.navigateTo({
-      url: "/pages/sub/login/index",
+      url: '/pages/sub/login/index',
     });
   };
   return (
-    <View
-      className={classNames(styles["login-btn"], className)}
-      onClick={login}
-    >
+    <View className={classNames(styles['login-btn'], className)} onClick={login}>
       <Image
-        src='https://aloss-qinghua-image.oss-cn-shanghai.aliyuncs.com/images/record_selected.png'
+        src="https://aloss-qinghua-image.oss-cn-shanghai.aliyuncs.com/images/record_selected.png"
         className={styles.icon}
       ></Image>
       <View>登录</View>
@@ -34,9 +31,6 @@ const Tabbar: FC<{ currentTab: number }> = ({ currentTab }) => {
     if (currentTab === index) return;
     switchTab(index);
   };
-  // useDidShow(() => {
-  //   Taro.hideTabBar().catch(() => 0);
-  // });
 
   useEffect(() => {
     checkLogin().then((res) => {
@@ -47,34 +41,29 @@ const Tabbar: FC<{ currentTab: number }> = ({ currentTab }) => {
   }, [_isLogin]);
 
   return (
-    <View className={`${styles.tabbar} safe-area-inset-bottom`}>
-      {tabs.map((item, index) => (
-        <View
-          key={item.pagePath}
-          className={`${styles.tabItem} ${
-            currentTab === index ? styles.active : ""
-          }`}
-          onClick={() => onClickItem(item, index)}
-          style={{
-            display: item.isLogin && !isLogin ? "none" : "flex",
-            width: `calc(100% / ${tabs.length})`,
-          }}
-        >
-          <Image
-            src={
-              currentTab === index
-                ? item.iconPath
-                : item.selectedIconPath
-            }
-            className={styles.icon}
-          />
-          <View className={styles.text}>{item.text}</View>
-        </View>
-      ))}
-      {!isLogin ? (
-        <LoginButton className={styles["login-btn-position"]} />
-      ) : null}
-    </View>
+    <>
+      <View className={styles.tabbar}>
+        {tabs.map((item, index) => (
+          <View
+            key={item.pagePath}
+            className={`${styles.tabItem} ${currentTab === index ? styles.active : ''}`}
+            onClick={() => onClickItem(item, index)}
+            style={{
+              display: item.isLogin && !isLogin ? 'none' : 'flex',
+              width: `calc(100% / ${tabs.length})`,
+            }}
+          >
+            <Image
+              src={currentTab === index ? item.iconPath : item.selectedIconPath}
+              className={styles.icon}
+            />
+            <View className={styles.text}>{item.text}</View>
+          </View>
+        ))}
+        {!isLogin ? <LoginButton className={styles['login-btn-position']} /> : null}
+      </View>
+      <View className="safe-area-inset-bottom"></View>
+    </>
   );
 };
 export default Tabbar;
--- src/store/systemStore.ts
+++ src/store/systemStore.ts
@@ -3,6 +3,7 @@ import Taro from '@tarojs/taro';
 import { create } from 'zustand';
 
 interface SystemState {
+  rpx: number;
   statusBarHeight: number;
   titleBarHeight: number;
   appBarHeight: number;
@@ -12,6 +13,7 @@ interface SystemState {
 }
 
 export const useSystemStore = create<SystemState>((set) => ({
+  rpx: 1,
   statusBarHeight: 0,
   titleBarHeight: 0,
   appBarHeight: 0,
@@ -20,7 +22,9 @@ export const useSystemStore = create<SystemState>((set) => ({
   initSystemInfo: () => {
     const systemInfo = Taro.getSystemInfoSync();
     const menuBtn = Taro.getMenuButtonBoundingClientRect();
+    const screenWidth = systemInfo.screenWidth;
     set({
+      rpx: screenWidth / 750,
       statusBarHeight: (systemInfo.statusBarHeight || 0) + 4,
       titleBarHeight: menuBtn.height,
       appBarHeight: (systemInfo.statusBarHeight || 0) + menuBtn.height,
